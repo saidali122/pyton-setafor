f6c642f0894e4f7f9da6436c97dfb47614b6c1eb
saidali122 <nuquljonjalilova@gmail.com>
2025-02-10 20:45:35 +0500

    pyton setafor

---
+++ main.py
@@ -0,0 +1,9 @@
+# while True:
+#     setafor = input("setafor qanaqa rang?>>")
+#
+#     if setafor == "qizil" or setafor == "sariq" or setafor == "yashil":
+#         print("Rahmat, to'g'ri keladi")
+#         break
+#     else:
+#         print("Xato rang!")
+
